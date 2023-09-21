969f629b1de8e26d02d961005d8791cc00a221aa
kibet nathan <nathanrufus540@gmail.com>
2023-09-21 17:25:56 +0300

    completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const names=['Guadalupe','Ollie','Aki'];
+const event = "surprise";
+ function writeCards( names, event ) {
+  let myCards = []
+  for ( let i = 0; i < names.length; i++ ) {
+    myCards.push( `Thank you, ${names[i]}, for the wonderful ${event} gift!` )
+  }
+  return myCards
+}
+
+function countDown(countNumber){
+    while(countNumber >= 0) {
+        console.log(countNumber);
+        countNumber--;
+    }
+}
+console.log(countDown(10))
